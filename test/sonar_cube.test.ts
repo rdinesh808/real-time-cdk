e798b87001370d1cc6c9bb99d22d2e70b663304a
Dinesh <rdinesh808@gmail.com>
2025-07-08 22:06:43 -0300

    Initial commit

---
+++ test/sonar_cube.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SonarCube from '../lib/sonar_cube-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sonar_cube-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SonarCube.SonarCubeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
